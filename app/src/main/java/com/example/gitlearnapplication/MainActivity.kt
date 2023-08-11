12264977fd5d4a2441b75640a2c09c3d94098686
savai_solanki <savai.solanki@brainvire.com>
2023-08-11 18:46:36 +0530

    Add Vijayanagar

--- app/src/main/java/com/example/gitlearnapplication/MainActivity.kt
+++ app/src/main/java/com/example/gitlearnapplication/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        println("Solanki Savai Sureshbhai")
+        println("Solanki Savai Sureshbhai  I am from Vijayanagar")
     }
 }
