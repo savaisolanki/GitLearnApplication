5087873d74a89c67ba708ae6a5394938a8fb4648
savai_solanki <savai.solanki@brainvire.com>
2023-08-11 18:52:20 +0530

    Commit cHANGE

--- app/src/main/java/com/example/gitlearnapplication/MainActivity.kt
+++ app/src/main/java/com/example/gitlearnapplication/MainActivity.kt
@@ -9,5 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("Solanki Savai Sureshbhai")
+        println("Solanki Savai Sureshbhai")
+        println("Solanki Savai Sureshbhai")
     }
 }
